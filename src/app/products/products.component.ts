1faeec1be61908cd6fce0dd0dfd494e0a939bbe6
Rohan <rohanakash48@gmail.com>
2022-05-24 11:51:02 +0530

    rohan

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CartService } from '../services/cart.service';
+import { ProductService } from '../services/product.service';
 
 export interface Cart{
   title?:string;
@@ -19,75 +20,23 @@ export class ProductsComponent implements OnInit {
 
   // public productList:any;
 
-  constructor(private cartservice:CartService) { }
+  constructor(private cartservice:CartService,private product:ProductService) { }
 
   ngOnInit(): void {
-
-    this.productList.forEach((a:any)=>{
-      Object.assign(a,{quantity:1,total:a.price});
-    });
+    this.getproduct()
   }
 
  
 
-  productList=
-  [
-    {
-      id:'1',
-      title:'1926 Rolls Royce Phantom Experimental Vehicle #10EX by Barker 1:18 Matrix scale model',
-      price:35555.00,
-      description:'The "Torpedo"10EX was built in 1926 as a sports car version of the Phantom 1, lapped Brooklands at an average speed of 91.2 mph',
-      category:'Luxury Car',
-      image:'https://cdn.shopify.com/s/files/1/0095/8211/6964/products/1926-Rolls-Royce-Phantom-Experimental-Vehicle-_10EX-by-Barker-1-18-Matrix-scale-model-car-collectible-miniature-1_grande.jpg?v=1641129093'
-    }
-    ,
-    {
-      id:'2',
-      title:'Rolls-Royce Phantom I light blue 1:18 Kyosho diecast model car',
-      price:24999.00,
-      description:'Rolls-Royce Phantom was replacement for the original Silver Ghost. Introduced as the New Phantom in 1925',
-      category:'Luxury Car',
-      image:'https://cdn.shopify.com/s/files/1/0095/8211/6964/products/Rolls-Royce-Phantom-I-1-18-Kyosho-scale-model-car-collectible-miniature-1_grande.jpg?v=1641832414'
-    },
-    {
-      id:'3',
-      title:'1998 Rolls-Royce Silver Seraph green 1:64 GFCC diecast scale miniature car',
-      price:4999.00,
-      description:'The Rolls-Royce Silver Seraph was a large luxury automobile produced by Rolls-Royce Motors from 1998 to 2002',
-      category:'Luxury Car',
-      image:'https://cdn.shopify.com/s/files/1/0095/8211/6964/products/1998-Rolls-Royce-Silver-Seraph-green-1-64-GFCC-diecast-karz-dolls-scale-model-car-1_grande.jpg?v=1642234539'
-    },
-    {
-      id:'4',
-      title:'Rolls-Royce Cullinan 1:64 DCM diecast scale model miniature car',
-      price:6899.00,
-      description:'Rolls-Royce Cullinan price is ₹ 6.95 Crore. Cullinan comes in 1 variant. The price of Cullinan petrol top version is ₹ 6.95 Crore.',
-      category:'Luxury Car',
-      image:'https://cdn.shopify.com/s/files/1/0095/8211/6964/products/Rolls-Royce-cullinan-1-64-dcm-diecast-official-scale-model-1_150x150.jpg?v=1638631896'
-    },
-    {
-      id:'5',
-      title:'Rolls Royce Phantom VIII white 1:64 diecast scale miniature car',
-      price:2699.00,
-      description:'Rolls-Royce Phantom VIII comes in 1 variant. The price of Phantom VIII petrol top version is ₹ 9.50 Crore.',
-      category:'Luxury Car',
-      image:'https://cdn.shopify.com/s/files/1/0095/8211/6964/products/Rolls-Royce-Phantom-VIII-white-1-64-small-car-art-diecast-scale-model-carz-dollz-1_150x150.jpg?v=1646128797'
-    },
-    {
+  
+    
       
-      id:'6',
-      title:'Rolls-Royce Wraith 1:64 LJM diecast scale model miniature car',
-      price:5899.00,
-      description:'The Wraith is a two-door coupé by Rolls-Royce. It was showcased at the 2013 Geneva Motor Show, Switzerland.',
-      category:'Luxury Car',
-      image:'https://cdn.shopify.com/s/files/1/0095/8211/6964/products/Rolls-Royce-Wraith-1-64-LJM-diecast-official-scale-model-1_150x150.jpg?v=1638631327'
-    }
+    
 
 
 
-  ]
   getproduct(){
-    this.cartservice.getPosts().subscribe((res:any)=>{
+    this.product.getPosts().subscribe((res:any)=>{
       this.datalist=res
     })
   }
@@ -107,6 +56,7 @@ export class ProductsComponent implements OnInit {
 
 
   }
+  
 
 
 
